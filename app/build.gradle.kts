110c8b52b24af60a2acfdbd3d9b735aada11ceba
Gabriel Hernandez <gabo982@gmail.com>
2026-07-17 18:06:10 -0600

    chore: bump app version to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.aiagents.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.3.2"
+        versionCode = 7
+        versionName = "0.3.3"
         buildConfigField("boolean", "EXTERNAL_VOICE_PACK", "false")
         buildConfigField("String", "VOICE_PACK_MANIFEST_URL", "\"\"")
 
